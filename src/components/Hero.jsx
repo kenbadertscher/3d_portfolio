757b815cac2e04c8407242d1b7aa3a77e6d22128
kenbadertscher <badertscher.ken@gmail.com>
2024-02-09 21:03:08 -0500

    Typo fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Ken</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop 3D visuals, user <br className="sm:block hidden"/>
-          interfaces, and web applciations.</p>
+          interfaces, and web applications.</p>
         </div>
       </div>
 
